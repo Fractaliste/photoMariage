fa9fa971ae13f8065621dd2cfec15640fb101567
Raphiki <Raphiki@192.168.1.25>
2014-11-16 14:21:32 +0100

    Création de l'IHM Mise en place du système de login Démarrage de l'interface d'ulpoad

--- src/RouteProvider.php
+++ src/RouteProvider.php
@@ -15,13 +15,75 @@ class RouteProvider implements ServiceProviderInterface {
 
     public function register(Application $app) {
 
+        $app->before(function (Request $request, Application $app) {
+            if ($request->getRequestUri() !== '/login' && $app['session']->get('password') !== 'ok') {
+                return $app->redirect('/login');
+            }
+        });
+
+        $app->get('/login', function () use ($app) {
+            return $app['twig']->render('password.twig');
+        });
+
+        $app->post('/login', function (Request $request) use ($app) {
+            if ($request->get('password') === '06122014') {
+                $app['session']->set('password', 'ok');
+                return $app->redirect('/');
+            } else {
+                return $app->redirect('/login');
+            }
+        });
 
         $app->get('/', function (Request $request) use ($app) {
-            return 'Bienvenue, ce site sera prochainement disponible !'.$request->getClientIp();
+            return $app['twig']->render('home.twig');
+        });
+
+        $app->get('/diaporama', function (Request $request) use ($app) {
+            return $app['twig']->render('diaporama.twig');
+        });
+
+        $app->get('/telecharger', function (Request $request) use ($app) {
+            return $app['twig']->render('telecharger.twig');
+        });
+
+        $app->get('/deposer', function (Request $request) use ($app) {
+            return $app['twig']->render('deposer.twig');
+        });
+
+        $app->post('/deposer', function (Request $request) use ($app) {
+            $photos = $request->files->get("photos");
+
+            $error = array();
+            foreach ($photos as $photo) {
+                $dir = $app['photo.dir'] . $request->request->get('folder') . '/';
+                $app['photo.save']($photo, $dir);
+            }
+            $filepath = print_r($dir, true);
+            return $app['twig']->render('deposer.twig', ['img' => $filepath]);
+        });
+
+        $app['photo.save'] = $app->protect(function ($photo, $dir) use ($app ) {
+            $result = preg_match('#^(.*)(\.(jpg|jpeg|png))$#i', $photo->getClientOriginalName(), $matches);
+            if (!$result) {
+                if ($result === false) {
+                    $app['logger']->addDebug('Problème de regex pour gérer le fichier : "' . $photo->getClientOriginalName() . '", l\'erreur retournée est : ' . preg_last_error());
+                } else {
+                    $error[] = 'Le fichier "' . $photo->getClientOriginalName() . '" n\'a pas pu être téléchargé, vérifiez qu\'il comporte bien une des extensions autorisées.';
+                }
+            } else {
+                $originalFileName = ucwords(strtolower($matches[1]));
+                $fileName = $originalFileName;
+                $i = 0;
+                while (file_exists($dir . $fileName . strtolower($matches[2]))) {
+                    $fileName = $originalFileName . '-' . ($i++);
+                }
+                $photo->move($dir, $fileName . strtolower($matches[2]));
+            }
         });
     }
 
     public function boot(Application $app) {
         
     }
+
 }
--- web/index.php
+++ web/index.php
@@ -3,6 +3,8 @@
 use Raphdine\LoggerProvider;
 use Raphdine\RouteProvider;
 use Silex\Application;
+use Silex\Provider\SessionServiceProvider;
+use Silex\Provider\TwigServiceProvider;
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
@@ -12,7 +14,14 @@ $app['debug'] = function() use ($app) {
     return $app['request_context']->getHost() === 'localhost';
 };
 
+$app['photo.dir']= __DIR__.'/img/mariage/';
+
+$app->register(new TwigServiceProvider(), array(
+    'twig.path' => __DIR__ . '/../src/vues',
+));
+
 $app->register(new LoggerProvider());
+$app->register(new SessionServiceProvider());
 $app->register(new RouteProvider());
 
 $app->run();
